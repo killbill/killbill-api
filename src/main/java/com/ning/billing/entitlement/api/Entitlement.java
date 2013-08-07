c1b416b781b30af9259ed09018b0b91ed512b176
stephane brossier <stephane@ning.com>
2013-08-07 15:03:19 -0700

    Initial entitlement code review with Pierre

--- src/main/java/com/ning/billing/entitlement/api/Entitlement.java
+++ src/main/java/com/ning/billing/entitlement/api/Entitlement.java
@@ -2,54 +2,256 @@ package com.ning.billing.entitlement.api;
 
 import com.ning.billing.catalog.api.ActionPolicy;
 import com.ning.billing.catalog.api.BillingPeriod;
+import com.ning.billing.catalog.api.Plan;
+import com.ning.billing.catalog.api.PlanPhase;
+import com.ning.billing.catalog.api.PriceList;
+import com.ning.billing.catalog.api.Product;
+import com.ning.billing.catalog.api.ProductCategory;
 import com.ning.billing.util.callcontext.CallContext;
 import org.joda.time.LocalDate;
 
+import java.util.List;
 import java.util.UUID;
 
+/**
+ * An Entitlement is created using the <code>EntitlementApi</code>
+ * <p/>
+ * It contains all the catalog information and current state that answers the entitlement question.
+ * <p>
+ * The users of that API will control all the entitlement behavior when making changes such as the effectiveDate, catalog info,...
+ * By default the system will use system wide policies to control the billing aspect, but specific APIs also allow to override those.
+ * <p>
+ * @see com.ning.billing.entitlement.api.EntitlementApi
+ */
 public interface Entitlement {
 
-
+    /**
+     * Used to control the effective date that should be used on Plan change or on cancellation.
+     */
     public enum EntitlementActionPolicy {
+        /* Start of Term. */
         SOT,
-        EOD
+        /* End of Term */
+        EOT
     }
 
+    /**
+     * Possible states of an <code>Entitlement</code>
+     */
+    public enum EntitlementState {
+        /* The entitlement was created in that initial state */
+        ACTIVE,
+        /* The user explicitely paused the entitlement */
+        PAUSED,
+        /* The system blocked the entitlement */
+        BLOCKED,
+        /* The user cancelled the entitlement */
+        CANCELLED
+    }
+
+    /**
+     * Possible <code>Entitlement</code> source types.
+     */
+    public enum EntitlementSourceType {
+        NATIVE,
+        MIGRATED,
+        TRANSFERRED
+    }
+
+    /**
+     * @return the unique id of the entitlement
+     */
     public UUID getId();
 
-    // Cancel entitlement at the specified date and let subscription/catalog/rules do the right thing for billing
+    /**
+     *
+     * @return the external key that was supplied when creating the base entitlement
+     */
+    public String getExternalKey();
+
+    /**
+     * @return the state of the entitlement
+     */
+    public EntitlementState getState();
+
+    /**
+     * @return the entitlement source type
+     */
+    public EntitlementSourceType getSourceType();
+
+    /**
+     * @return the start date of the entitlement
+     */
+    public LocalDate getEffectiveStartDate();
+
+    /**
+     * @return the end date of the entitlement, that is the date at which it got cancelled.
+     */
+    public LocalDate getEffectiveEndDate();
+
+    /**
+     * @return the requested end date of the entitlement, that is the date at which the user submitted the cancellation.
+     */
+    public LocalDate getRequestedEndDate();
+
+    /**
+     * @return the current <code>Product</code>
+     */
+    public Product getProduct();
+
+    /**
+     * @return the current <code>Plan</code>
+     */
+    public Plan getPlan();
+
+    /**
+     * @return the current <code>PriceList></code>
+     */
+    public PriceList getPriceList();
+
+    /**
+     * @return the current <Phase></Phase>
+     */
+    public PlanPhase getCurrentPhase();
+
+    /**
+     * @return the current <code>ProductCategory</code>
+     */
+    public ProductCategory getProductCategory();
+
+    /**
+     * @return the last <code>Product</code> prior to cancellation
+     */
+    public Product getLastActiveProduct();
+
+    /**
+     * @return the last <code>Plan</code> prior to cancellation
+     */
+    public Plan getLastActivePlan();
+
+    /**
+     * @return the last active PriceList
+     */
+    public PriceList getLastActivePriceList();
+
+    /**
+     * @return the last active ProductCategory
+     */
+    public String getLastActiveProductCategory();
+
+
+    /**
+     * Cancels the <code>Entitlement</code> at the specified date
+     * <p/>
+     * The date is interpreted by the system to be in the timezone specified at the <code>Account</code>
+     *
+     * @param effectiveDate the date at which the entitlement should be cancelled
+     * @param context       the context
+     * @return true if the entitlement is in the <tt>CANCELLED</tt> state
+     * @throws EntitlementApiException if cancellation failed
+     */
     public boolean cancelEntitlementWithDate(final LocalDate effectiveDate, final CallContext context)
             throws EntitlementApiException;
 
-    // Cancel entitlement at a date that matches SOT/EOT (billing)
+
+    /**
+     * Cancel the <code>Entitlement</code> with a policy.
+     *
+     * @param policy  the policy that is used by the system to calculate the cancellation date
+     * @param context the context
+     * @return true if the entitlement is in the <tt>CANCELLED</tt> state
+     * @throws EntitlementApiException if cancellation failed
+     */
     public boolean cancelEntitlementWithPolicy(final EntitlementActionPolicy policy, final CallContext context)
             throws EntitlementApiException;
 
-    // Cancel entitlement at the specified date and override subscription/catalog/rules
-    public boolean cancel(final LocalDate effectiveDate, final ActionPolicy billingPolicy, final CallContext context)
-            throws EntitlementApiException;
 
-    // Cancel entitlement at a date that matches SOT/EOT (billing) and override subscription/catalog/rules
-    public boolean cancelEntitlementWithDateOverrideBillingPolicy(final EntitlementActionPolicy policy, final ActionPolicy billingPolicy, final CallContext context)
-            throws EntitlementApiException;
-
-
-    // Uncancel entitlement (only works if entitlement is maked as canceled in the future)
-    public boolean uncancel(final CallContext context)
-            throws EntitlementApiException;
-
-
-    public boolean changePlan(final String productName, final BillingPeriod term, final String priceList, final LocalDate requestedDate, final CallContext context)
-            throws EntitlementApiException;
-
-    public boolean changePlanOverrideBillingPolicy(final String productName, final BillingPeriod term, final String priceList, final LocalDate requestedDate,
-                                        final ActionPolicy policy, final CallContext context)
+    /**
+     * Cancels the <code>Entitlement</code> at the specified date
+     * <p/>
+     * The date is interpreted by the system to be in the timezone specified at the <code>Account</code>
+     *
+     * @param effectiveDate the date at which the entitlement should be cancelled
+     * @param billingPolicy the billingPolicy
+     * @param context       the context
+     * @return true if the entitlement is in the <tt>CANCELLED</tt> state
+     * @throws EntitlementApiException if cancellation failed
+     */
+    public boolean cancelEntitlementWithDateOverrideBillingPolicy(final LocalDate effectiveDate, final ActionPolicy billingPolicy, final CallContext context)
             throws EntitlementApiException;
 
 
+    /**
+     * Cancels the <code>Entitlement</code> at the specified date and overrides the default billing policy.
+     *
+     * @param policy        the policy that is used by the system to calculate the cancellation date
+     * @param billingPolicy the override billing policy
+     * @param context       the context
+     * @return true if the entitlement is in the <tt>CANCELLED</tt> state
+     * @throws EntitlementApiException if cancellation failed
+     */
+    public boolean cancelEntitlementWithPolicyOverrideBillingPolicy(final EntitlementActionPolicy policy, final ActionPolicy billingPolicy, final CallContext context)
+            throws EntitlementApiException;
+
+
+    /**
+     * Change <code>Entitlement</code> plan at the specified date.
+     * <p/>
+     * The date is interpreted by the system to be in the timezone specified at the <code>Account</code>
+     *
+     * @param productName   the new product name
+     * @param billingPeriod the new billing period
+     * @param priceList     the new priceList
+     * @param effectiveDate the date at which the entitlement should be changed
+     * @param context       the context
+     * @return true if the change has already taken effect
+     * @throws EntitlementApiException if change failed
+     */
+    public boolean changePlan(final String productName, final BillingPeriod billingPeriod, final String priceList, final LocalDate effectiveDate, final CallContext context)
+            throws EntitlementApiException;
+
+    /**
+     * Change <code>Entitlement</code> plan at the specified date and overrides the billing policy.
+     * <p/>
+     * The date is interpreted by the system to be in the timezone specified at the <code>Account</code>
+     *
+     * @param productName   the new product name
+     * @param billingPeriod the new billing period
+     * @param priceList     the new priceList
+     * @param effectiveDate the date at which the entitlement should be changed
+     * @param billingPolicy the override billing policy
+     * @param context       the context
+     * @return true if the change has already taken effect
+     * @throws EntitlementApiException if change failed
+     */
+    public boolean changePlanOverrideBillingPolicy(final String productName, final BillingPeriod billingPeriod, final String priceList, final LocalDate effectiveDate,
+                                                   final ActionPolicy billingPolicy, final CallContext context)
+            throws EntitlementApiException;
+
+
+    /**
+     * Pauses an <code>Entitlement</code> until it gets resumed.
+     * <p/>
+     * The date is interpreted by the system to be in the timezone specified at the <code>Account</code>
+     *
+     * @param effectiveDate the date at which the entitlement should be paused
+     * @param context       the context
+     * @return true if the entitlement has been paused
+     * @throws EntitlementApiException if the entitlement was not in <tt>ACTIVE</tt> state
+     */
     public boolean pause(final LocalDate effectiveDate, final CallContext context)
             throws EntitlementApiException;
 
+    /**
+     * Resumes an <code>Entitlement</code> that was paused
+     * <p/>
+     * The date is interpreted by the system to be in the timezone specified at the <code>Account</code>
+     *
+     * @param effectiveDate the date at which the entitlement should be resumed
+     * @param context       the context
+     * @return true if the entitlement has been resumed
+     * @throws EntitlementApiException
+     */
     public boolean resume(final LocalDate effectiveDate, final CallContext context)
             throws EntitlementApiException;
 }
--- src/main/java/com/ning/billing/entitlement/api/EntitlementApi.java
+++ src/main/java/com/ning/billing/entitlement/api/EntitlementApi.java
@@ -16,34 +16,168 @@
 
 package com.ning.billing.entitlement.api;
 
+import com.ning.billing.catalog.api.ActionPolicy;
 import com.ning.billing.catalog.api.PlanPhaseSpecifier;
 import com.ning.billing.util.callcontext.CallContext;
 import com.ning.billing.util.callcontext.TenantContext;
+import org.joda.time.LocalDate;
 
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * Primary API to manage the creation and retrieval of <code>Entitlement</code>.
+ *
+ */
 public interface EntitlementApi {
 
+    /**
+     *
+     * Create a new entitlement for that account.
+     * <p>
+     * The <code>PlanPhaseSpecifier<code/> should refer to a <code>ProductCategory.BASE</code> of
+     * <code>ProductCategory.STANDALONE</code>.
+     *
+     * @param accountId     the account id
+     * @param spec          the product specification for that new entitlement
+     * @param externalKey   the external key for that entitlement
+     * @param context       the context
+     * @return              a new entitlement
+     *
+     * @throws EntitlementApiException if the system fail to create the <code>Entitlement</code>
+     */
     public Entitlement createBaseEntitlement(UUID accountId, PlanPhaseSpecifier spec, String externalKey, CallContext context)
             throws EntitlementApiException;
 
+    /**
+     *
+     * Adds an ADD_ON entitlement to previously created entitlement.
+     * <p>
+     * The <code>PlanPhaseSpecifier<code/> should refer to a <code>ProductCategory.ADD_ON</code>.
+     * The new entitlement will be bundled using the externalKey that was specified when creating the
+     * base entitlement.
+     *
+     * @param baseEntitlementId the id of the base entitlement
+     * @param spec              the product specification for that new entitlement
+     * @param context           the context
+     * @return                  a new entitlement
+     *
+     * @throws EntitlementApiException if the system fail to create the <code>Entitlement</code>
+     */
     public Entitlement addEntitlement(UUID baseEntitlementId, PlanPhaseSpecifier spec, CallContext context)
             throws EntitlementApiException;
 
+    /**
+     *
+     * Retrieves an <code>Entitlement</code> using its id.
+     *
+     * @param id        the id of the entitlement
+     * @param context   the context
+     * @return          the entitlement
+     *
+     * @throws EntitlementApiException if the entitlement does not exist
+     */
     public Entitlement getEntitlementFromId(UUID id, TenantContext context) throws EntitlementApiException;
 
-    public List<Entitlement> getAllEntitlementFromBaseId(UUID baseEntitlementId, TenantContext context) throws EntitlementApiException;
+    /**
+     *
+     * Retrieves all the <code>Entitlement</code> attached to the base entitlement.
+     *
+     * @param baseEntitlementId     the id of the base entitlement
+     * @param context               the context
+     * @return                      a list of entitlements
+     *
+     */
+    public List<Entitlement> getAllEntitlementsFromBaseId(UUID baseEntitlementId, TenantContext context);
 
-    public List<Entitlement> getAllEntitlementForAccountIdAndExternalKey(UUID accountId, String externalKey, TenantContext context) throws EntitlementApiException;
+    /**
+     *
+     * Retrieves all the <code>Entitlement</code> for a given account and matching an external key.
+     *
+     * @param accountId     the account id
+     * @param externalKey   the external key
+     * @param context       the context
+     *
+     * @return              a list of entitlements
+     *
+     * @throws EntitlementApiException if the account does not exist
+     */
+    public List<Entitlement> getAllEntitlementsForAccountIdAndExternalKey(UUID accountId, String externalKey, TenantContext context) throws EntitlementApiException;
 
-    public List<Entitlement> getAllEntitlementFromAccountId(UUID accountId, TenantContext context) throws EntitlementApiException;
+    /**
+     *
+     * Retrieves all the <code>Entitlement</code> for a given account.
+     *
+     * @param accountId     the account id
+     * @param context       the context
+     *
+     * @return              a list of entitlements
+     *
+     * @throws EntitlementApiException if the account does not exist
+     */
+    public List<Entitlement> getAllEntitlementsFromAccountId(UUID accountId, TenantContext context) throws EntitlementApiException;
+
+    /**
+     * Transfer all the <code>Enitlement</code> from the source account and matching the external key to the destination account.
+     * <p>
+     * The date is interpreted by the system to be in the timezone specified at the destination <code>Account</code>.
+     * <p>
+     * The <code>Entitlement</code> on the source account will be cancelled at effective date and the <code>Entitlement</code>
+     * on the destination account will be created at the effectiveDate.
+     *
+     * @param sourceAccountId   the unique id for the account on which the bundle will be transferred from
+     * @param destAccountId     the unique id for the account on which the bundle will be transferred to
+     * @param externalKey       the externalKey for the bundle
+     * @param effectiveDate     the date at which this transfer should occur
+     * @param context           the user context
+     *
+     * @return                  the id of the newly created base entitlement for the destination account
+     *
+     * @throws EntitlementApiException if the system could not transfer the entitlements
+     *
+     */
+    public UUID transferEntitlements(final UUID sourceAccountId, final UUID destAccountId, final String externalKey, final LocalDate effectiveDate,
+                                     final CallContext context)
+            throws EntitlementApiException;
+
+    /**
+     * Transfer all the <code>Entitlement</code> from the source account and matching the external key to the destination account.
+     * <p>
+     * The date is interpreted by the system to be in the timezone specified at the destination <code>Account</code>.
+     * <p>
+     * The <code>Entitlement</code> on the source account will be cancelled at effective date and the <code>Entitlement</code>
+     * on the destination account will be created at the effectiveDate. The <tt>billingPolicy</tt> will be used to override
+     * the default billing behavior for the cancellation of the subscriptions on the source account.
+     *
+     *
+     * @param sourceAccountId   the unique id for the account on which the bundle will be transferred from
+     * @param destAccountId     the unique id for the account on which the bundle will be transferred to
+     * @param externalKey       the externalKey for the bundle
+     * @param effectiveDate     the date at which this transfer should occur
+     * @param billingPolicy     the override billing policy
+     * @param context           the user context
+     *
+     * @return                  the id of the newly created base entitlement for the destination account
+     *
+     * @throws EntitlementApiException if the system could not transfer the entitlements
+     *
+     */
+    public UUID transferEntitlementsOverrideBillingPolicy(final UUID sourceAccountId, final UUID destAccountId, final String externalKey, final LocalDate effectiveDate,
+                                                          final ActionPolicy billingPolicy, final CallContext context)
+            throws EntitlementApiException;
 
 
     /**
-     * @param overdueableId the uuid of the object potentially in an overduabke state
-     * @param context       the context associated to that call
-     * @return a list of all the blocking states for that object
+     * Returns an <code>EntitlementBundleTimeline</code> for the set of <code>Entitlement</code> associated with the account and external key.
+     *
+     * @param accountId     the account id
+     * @param externalKey   the external key
+     * @param context       the context
+     *
+     * @return              the timeline
+     *
+     * @throws EntitlementApiException there is not base entitlement for that account and external key
      */
-    public List<BlockingState> getBlockingHistory(UUID overdueableId, TenantContext context);
+    public EntitlementBundleTimeline getEntitlementBundleTimeline(UUID accountId, String externalKey, TenantContext context)
+        throws EntitlementApiException;
 }
