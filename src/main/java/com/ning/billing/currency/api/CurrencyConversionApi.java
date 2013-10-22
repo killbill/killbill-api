66bf4fd955ca2649ddc01e7eb3107da49b121651
stephane brossier <stephane@ning.com>
2013-10-22 16:35:31 -0700

    Add initial currency conversion api

---
+++ src/main/java/com/ning/billing/currency/api/CurrencyConversionApi.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2010-2013 Ning, Inc.
+ *
+ * Ning licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.ning.billing.currency.api;
+
+import java.util.List;
+
+import org.joda.time.DateTime;
+
+import com.ning.billing.catalog.api.Currency;
+
+public interface CurrencyConversionApi {
+
+    /**
+     *
+     * @return the list of currency that can be used as a base currency for conversion
+     */
+    public List<Currency> getBaseRates();
+
+    /**
+     * @param baseCurrency                 the base currency to be used
+     * @return                             the currency list of currency conversion rates
+     * @throws CurrencyConversionException if baseCurrency is not supported
+     */
+    public CurrencyConversion getCurrentCurrencyConversion(Currency baseCurrency)
+        throws CurrencyConversionException;
+
+    /**
+     * @param baseCurrency                 the base currency to be used
+     * @param dateConversion               the date for the conversion
+     * @return                             the list of currency conversion rates for the date provided
+     * @throws CurrencyConversionException if baseCurrency is not supported
+     */
+    public CurrencyConversion getCurrencyConversion(Currency baseCurrency, DateTime dateConversion)
+            throws CurrencyConversionException;
+}
