47361385d02b8f6215a4b11588613a1ee60e3e7c
Stéphane Brossier <stephane@kill-bill.org>
2015-06-16 10:18:18 -0700

    Minor comment edits

--- src/main/java/org/killbill/billing/payment/api/TransactionStatus.java
+++ src/main/java/org/killbill/billing/payment/api/TransactionStatus.java
@@ -23,11 +23,11 @@ public enum TransactionStatus {
     SUCCESS,
     /* Initial status or plugin does not know (transaction may or not have happenend) */
     UNKNOWN,
-    /* The payment is asynchronous and final state will be updated later */
+    /* The payment transaction is asynchronous and final state will be updated later */
     PENDING,
-    /* Transaction went through, but came back with an error (for e.g : not enough funds) */
+    /* The payment transaction went through, but came back with an error (for e.g : not enough funds) */
     PAYMENT_FAILURE,
-    /* The transaction did not go through, and plugin knows for sure that operation was not even attenpted (for e.g: connection error to gateway) */
+    /* The payment transaction did not go through, and plugin knows for sure that operation was not even attenpted (for e.g: connection error to gateway) */
     PLUGIN_FAILURE,
     /* Payment Subsystem is off */
     PAYMENT_SYSTEM_OFF;
