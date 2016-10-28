6ee4ab50d57c70d31301c939552f30527602b1ae
Sruthi Pendyala <SruthiPendyala@carfax.com>
2016-10-28 13:41:14 -0400

    Usage price override #614 TieredBlockPrice extends BlockPrice!

--- src/main/java/org/killbill/billing/catalog/api/TieredBlockPriceOverride.java
+++ src/main/java/org/killbill/billing/catalog/api/TieredBlockPriceOverride.java
@@ -3,7 +3,8 @@ package org.killbill.billing.catalog.api;
 /**
  * Created by sruthipendyala on 10/17/16.
  */
-public interface TieredBlockPriceOverride {
+
+public interface TieredBlockPriceOverride extends BlockPriceOverride {
     /**
      * @return the max used usage to consider this tiered block.
      */
