6abdb52db07cf71132ea843f7ebc091045dd1c69
Pierre-Alexandre Meyer <pierre@mouraf.org>
2018-06-12 16:19:20 -0700

    invoice: expose payment APIs
    See https://github.com/killbill/killbill/issues/981.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- src/main/java/org/killbill/billing/invoice/api/InvoicePaymentApi.java
+++ src/main/java/org/killbill/billing/invoice/api/InvoicePaymentApi.java
@@ -1,7 +1,9 @@
 /*
  * Copyright 2010-2013 Ning, Inc.
+ * Copyright 2014-2018 Groupon, Inc
+ * Copyright 2014-2018 The Billing Project, LLC
  *
- * Ning licenses this file to you under the Apache License, version 2.0
+ * The Billing Project licenses this file to you under the Apache License, version 2.0
  * (the "License"); you may not use this file except in compliance with the
  * License.  You may obtain a copy of the License at:
  *
@@ -16,13 +18,126 @@
 
 package org.killbill.billing.invoice.api;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
+import org.joda.time.DateTime;
 import org.killbill.billing.KillbillApi;
+import org.killbill.billing.account.api.Account;
+import org.killbill.billing.catalog.api.Currency;
+import org.killbill.billing.payment.api.PaymentApiException;
+import org.killbill.billing.payment.api.PaymentOptions;
+import org.killbill.billing.payment.api.PluginProperty;
+import org.killbill.billing.security.RequiresPermissions;
+import org.killbill.billing.util.callcontext.CallContext;
 import org.killbill.billing.util.callcontext.TenantContext;
 
+import static org.killbill.billing.security.Permission.PAYMENT_CAN_REFUND;
+import static org.killbill.billing.security.Permission.PAYMENT_CAN_TRIGGER_PAYMENT;
+
 public interface InvoicePaymentApi extends KillbillApi {
+
+    /**
+     * Combine an authorize and capture payment for a given invoice and allow to go through registered routing plugins
+     *
+     * @param account                       the account
+     * @param invoiceId                     the invoice id
+     * @param paymentMethodId               the payment method id to use
+     * @param paymentId                     the payment id (non-null for multi-steps flows, such as 3D Secure)
+     * @param amount                        the amount to pay
+     * @param currency                      the amount currency
+     * @param effectiveDate                 the effectiveDate of the payment operation
+     * @param paymentExternalKey            the payment external key
+     * @param paymentTransactionExternalKey the payment transaction external key
+     * @param properties                    plugin specific properties
+     * @param paymentOptions                options to control payment behavior
+     * @param context                       the call context
+     * @return the invoice payment
+     * @throws PaymentApiException
+     */
+    @RequiresPermissions(PAYMENT_CAN_TRIGGER_PAYMENT)
+    public InvoicePayment createPurchaseForInvoice(Account account,
+                                                   UUID invoiceId,
+                                                   UUID paymentMethodId,
+                                                   UUID paymentId,
+                                                   BigDecimal amount,
+                                                   Currency currency,
+                                                   DateTime effectiveDate,
+                                                   String paymentExternalKey,
+                                                   String paymentTransactionExternalKey,
+                                                   Iterable<PluginProperty> properties,
+                                                   PaymentOptions paymentOptions,
+                                                   CallContext context) throws PaymentApiException;
+
+    /**
+     * Refund a previously captured payment and allow to go through registered routing  plugins
+     *
+     * @param isAdjusted                    whether to adjust the invoice
+     * @param adjustments                   individual adjustments per invoice item id
+     * @param account                       the account
+     * @param paymentId                     the payment id
+     * @param effectiveDate                 the effectiveDate of the payment operation
+     * @param amount                        the amount to refund
+     * @param currency                      the amount currency
+     * @param paymentTransactionExternalKey the payment transaction external key
+     * @param properties                    plugin specific properties
+     * @param paymentOptions                options to control payment behavior
+     * @param context                       the call context
+     * @return the payment
+     * @throws PaymentApiException
+     */
+    @RequiresPermissions(PAYMENT_CAN_REFUND)
+    public InvoicePayment createRefundForInvoice(boolean isAdjusted,
+                                                 Map<UUID, BigDecimal> adjustments,
+                                                 Account account,
+                                                 UUID paymentId,
+                                                 BigDecimal amount,
+                                                 Currency currency,
+                                                 DateTime effectiveDate,
+                                                 String paymentTransactionExternalKey,
+                                                 Iterable<PluginProperty> properties,
+                                                 PaymentOptions paymentOptions,
+                                                 CallContext context) throws PaymentApiException;
+
+    /**
+     * Credit a payment method for a given invoice and allow to go through registered routing  plugins
+     * <p/>
+     * This is also known as payment in reverse.
+     *
+     * @param isAdjusted                    whether to adjust the invoice
+     * @param adjustments                   individual adjustments per invoice item id
+     * @param account                       the account
+     * @param originalPaymentId             the original payment id
+     * @param paymentMethodId               the payment method id to use
+     * @param paymentId                     the payment id (non-null for multi-steps flows)
+     * @param amount                        the amount to credit
+     * @param currency                      the amount currency
+     * @param effectiveDate                 the effectiveDate of the payment operation
+     * @param paymentExternalKey            the payment external key
+     * @param paymentTransactionExternalKey the payment transaction external key
+     * @param properties                    plugin specific properties
+     * @param paymentOptions                options to control payment behavior
+     * @param context                       the call context
+     * @return the payment
+     * @throws PaymentApiException
+     */
+    @RequiresPermissions(PAYMENT_CAN_TRIGGER_PAYMENT)
+    public InvoicePayment createCreditForInvoice(boolean isAdjusted,
+                                                 Map<UUID, BigDecimal> adjustments,
+                                                 Account account,
+                                                 UUID originalPaymentId,
+                                                 UUID paymentMethodId,
+                                                 UUID paymentId,
+                                                 BigDecimal amount,
+                                                 Currency currency,
+                                                 DateTime effectiveDate,
+                                                 String paymentExternalKey,
+                                                 String paymentTransactionExternalKey,
+                                                 Iterable<PluginProperty> properties,
+                                                 PaymentOptions paymentOptions,
+                                                 CallContext context) throws PaymentApiException;
 
     public List<InvoicePayment> getInvoicePayments(UUID paymentId, TenantContext context);
 
